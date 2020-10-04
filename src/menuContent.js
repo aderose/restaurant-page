103c9b23f7acbc3b15b2a7388cf0f0ab62127be3
aderose <sandro.derose28@yahoo.com>
2020-10-04 20:47:17 +0100

    Added example menu items and formatted

--- src/menuContent.js
+++ src/menuContent.js
@@ -11,8 +11,7 @@ function createCopy() {
   const copy = document.createElement("div");
   copy.setAttribute("class", "copy container");
   copy.appendChild(createHeadline());
-  const menu = createMenu();
-  copy.appendChild(menu);
+  copy.appendChild(createMenu());
   return copy;
 }
 
@@ -28,7 +27,64 @@ function createHeadline() {
 
 // create menu
 function createMenu() {
-  return document.createElement("div");
+  const menu = document.createElement("div");
+  const options = [
+    {
+      name: "Salad & Shrimp",
+      price: "£10.00",
+      description: "A beautiful mix of grilled shrimp and vegetables.",
+    },
+    {
+      name: "Salad & Shrimp",
+      price: "£10.00",
+      description: "A beautiful mix of grilled shrimp and vegetables.",
+    },
+    {
+      name: "Salad & Shrimp",
+      price: "£10.00",
+      description: "A beautiful mix of grilled shrimp and vegetables.",
+    },
+    {
+      name: "Salad & Shrimp",
+      price: "£10.00",
+      description: "A beautiful mix of grilled shrimp and vegetables.",
+    },
+    {
+      name: "Salad & Shrimp",
+      price: "£10.00",
+      description: "A beautiful mix of grilled shrimp and vegetables.",
+    },
+  ];
+  options.forEach((option) => menu.appendChild(createOption(option)));
+  return menu;
+}
+
+function createOption(optionObj) {
+  const option = document.createElement("div");
+  option.setAttribute("class", "meal-container");
+
+  const optionHeader = document.createElement("div");
+  optionHeader.setAttribute("class", "meal-header");
+
+  const title = document.createElement("div");
+  title.textContent = optionObj.name;
+  title.setAttribute("class", "meal-title");
+
+  const price = document.createElement("div");
+  price.textContent = optionObj.price;
+  price.setAttribute("class", "meal-price");
+
+  optionHeader.appendChild(title);
+  optionHeader.appendChild(price);
+
+  const description = document.createElement("p");
+  description.textContent = optionObj.description;
+  description.setAttribute("class", "meal-desc");
+
+  option.appendChild(optionHeader);
+  option.appendChild(description);
+
+  return option;
 }
 
 export { menuContent };
